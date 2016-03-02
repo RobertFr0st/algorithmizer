db2479d997a5d69298b72d20f9024bfcb4996038
Nick Kelley <ndk.1995@gmail.com>
2016-03-02 00:18:58 -0500

    nested circle and text within group tag

--- circle_data.js
+++ circle_data.js
@@ -1,18 +1,33 @@
 function create_circles() {
   var canvas_height = 200;
   var canvas_width = 300;
+  var canvas_border = 1;
 
   var dataset = [10, 20, 30, 40, 100, 79];
 
-  var svg = d3.select("body")
+  var svg_container = d3.select("body")
     .append("svg")
     .attr("width", canvas_width)
-    .attr("height", canvas_height);
+    .attr("height", canvas_height)
+    .attr("border", canvas_border);
+  var circle_text_group = svg_container.append("g");
+
+  //this is a bit buggy can't get it not to be a solid black box but
+  //uncomment to see the size of your canvas
+  //for debugging purposes this box represents the edges of our canvas
+  /*var borderPath = svg_container.append("rect")
+    .attr("x", 0)
+    .attr("y", 0)
+    .attr("height", canvas_height)
+    .attr("width", canvas_width)
+    .style("stroke", bordercolor)
+    .style("fill", "blue")
+    .style("stroke-width", 1);*/
 
   //circle varables
   var radius = 20;
   var padding = 2;
-  var circle = svg.selectAll("circle");
+  var circle = circle_text_group.selectAll("circle");
 
   //text variables
   //this parameter is a hacky way of specifing font size.
@@ -37,7 +52,7 @@ function create_circles() {
       }
     });
 
-  svg.selectAll("text")
+  circle_text_group.selectAll("text")
     .data(dataset)
     .enter()
     .append("text")
