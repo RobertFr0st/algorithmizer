7c4e0d505da14b9edc7907723d43cfd801cdaf58
Nick Kelley <ndk.1995@gmail.com>
2016-03-14 14:38:00 -0400

    set global variables to default values and added event listner functions

---
+++ visualization.js
@@ -0,0 +1,40 @@
+// custom variables
+var w = 500, h = 190,
+    element_count = 15, speed = 50,
+    implimentation = "selection",
+    dataset, scale, padding = 2, timer,
+    states = {"default": 0, "finished": 1, "current": 2, "compare": 3, "minimal": 4, "hide": 5},
+    colors = ["#B7C4CF", "#3565A1", "#D55511", "#74A82A", "#A42F11", "#fff"],
+    svg;
+
+
+
+//choice logic, will be removed since we have 1 implimentation per page
+document.getElementById("implimentation").addEventListener("change", function() {
+    implimentation = this.value;
+});
+
+//reset button
+document.getElementById("reset").addEventListener("click", function() {
+  //interrupt previous play
+  clearInterval(timer);
+
+  //set speed and count to input values
+  element_count = document.getElementById("element_count").value;
+  speed = document.getElementById("speed").value;
+
+  setDataset(element_count);
+  setRects(dataset);
+});
+
+//play
+document.getElementById("play").addEventListener("click", function() {
+  //inturrupt previous play
+  clearInterval(timer);
+
+  //reset states
+  for (var i = dataset.length - 1; i >= 0; i--)
+    dataset[i].state = states.default;
+
+  sorts[implimentation]();
+});
